c08f468db8941b783d19572d6623ec1984f77ff1
Buwaneka De Silva <buwanekadesilva@Buwanekas-MacBook-Air.local>
2018-05-12 11:37:40 +0530

    Added UninstallTeamViewer.sh

---
+++ UninstallTeamViewer.sh
@@ -0,0 +1,14 @@
+echo "Uninstalling TeamViewer Studio From Mac..."
+sudo rm -Rf /Applications/Android\ TeamViewer.app
+sudo rm -Rf ~/Library/Preferences/TeamViewer*
+sudo rm -Rf ~/Library/Application Support/TeamViewer
+sudo rm -Rf ~/Library/Caches/ com.teamviewer.TeamViewer
+sudo rm -Rf ~/Library/Preferences/com.teamviewer10.plist
+sudo rm ~/Library/Preferences/com.teamviewer.TeamViewer.plist
+sudo rm ~/Library/Preferences/com.teamviewer.teamviewer.preferences.Machine.plist
+sudo rm ~/Library/Preferences/com.teamviewer.teamviewer.preferences.plist
+sudo rm -Rf ~/Library/Application/TeamViewer*
+sudo rm -Rf ~/Library/Logs/TeamViewer*
+sudo rm -Rf ~/Library/Caches/TeamViewer*
+sudo rm -Rf ~/Library/TeamViewer*
+echo "Uninstallation of TeamViewer From Mac has been completed"
