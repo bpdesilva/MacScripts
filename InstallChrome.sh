ddeb41ff9c6262537f69ac332ca6d5a48d148f5a
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-05-16 00:22:41 +0530

    Download Chrome
    Add chrome

---
+++ InstallChrome.sh
@@ -0,0 +1,4 @@
+#Reference Link : https://superuser.com/questions/602680/how-to-install-google-chrome-from-the-command-line
+wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+open ~/Downloads/googlechrome.dmg
+sudo cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
