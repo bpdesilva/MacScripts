08f64256fa5ffa8a284a86fe8bca14c7469b03f7
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-05-21 01:17:41 +0530

    Added
    New File

---
+++ InstallAndroidStudio.sh
@@ -0,0 +1,3 @@
+wget https://dl.google.com/dl/android/studio/install/3.1.2.0/android-studio-ide-173.4720617-mac.dmg
+open ~/Downloads/android-studio-ide-173.4720617-mac.dmg
+sudo cp -r /Volumes/Google\ Android Studio/Google\ Android Studio.app /Applications/
