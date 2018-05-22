a4ab490a090a33216b451c71053aa4e6e2272b55
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-05-22 13:06:46 +0530

    Install Node  & Homebrew
    Added Files

---
+++ InstallNode.sh
@@ -0,0 +1,5 @@
+echo "Installing Homebrew"
+sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing Node"
+sudo brew install node
+echo "Node Installed"
