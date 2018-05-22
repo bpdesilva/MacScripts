562d493dbdc67a974b8d1cc11145f43d9eac4e3b
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-05-23 04:03:45 +0530

    Updated Files
    Rename
    Rectifications

--- GetFlutter.sh
+++ GetFlutter.sh
@@ -1,7 +1,7 @@
 echo "Changing Directory to Downloads...."
 sudo cd ~/download
 echo "Downloading Flutter....."
-wget https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.3.2-beta.zip
+curl -LO https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.3.2-beta.zip
 echo "Extracting Flutter........."
 unzip flutter_macos_v0.3.2-beta.zip
 echo "Settting up enviroment....."
