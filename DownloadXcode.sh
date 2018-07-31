5c53770ccbc13d10d6b2f5311829506a845413ba
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-07-31 16:33:16 +0530

    Update DownloadXcode.sh

--- DownloadXcode.sh
+++ DownloadXcode.sh
@@ -2,6 +2,6 @@ echo "Do you wish to Download Xcode Standalone Manually?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) /usr/bin/open -a "/Applications/Google Chrome.app" 'https://developer.apple.com/download/more/'; break;;
-        No ) echo "Download Xcode Standalone automatically...."; /usr/bin/open -a "/Applications/Google Chrome.app" 'https://developer.apple.com/services-account/download?path=/Developer_Tools/Xcode_9.4.1/Xcode_9.4.1.xip'; break;;
+        No ) echo "Download Xcode Standalone automatically...."; /usr/bin/open -a "/Applications/Google Chrome.app" 'https://developer.apple.com/services-account/download?path=/Developer_Tools/Xcode_9.4.1/Xcode_9.4.1.xip'; exit;;
     esac
 done
