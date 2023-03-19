fd387f301ef235f02678a6fb82da7c1d792fb8fc
Buwaneka De Silva <19805825+bpdesilva@users.noreply.github.com>
2023-03-19 16:09:01 +0530

    Add files via upload

---
+++ Util/disablesleep.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo pmset -a sleep 0
+sudo pmset -a hibernatemode 0
+sudo pmset -a disablesleep 1
---
+++ Util/enablesleep.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo pmset -a sleep 1
+sudo pmset -a hibernatemode 3
+sudo pmset -a disablesleep 0
